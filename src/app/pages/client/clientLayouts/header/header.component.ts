71b26110af3ecda4efd5308ada574707c48325d4
ducptd98 <ducptd98@gmail.com>
2020-09-17 17:40:41 +0700

    home page

--- src/app/pages/client/clientLayouts/header/header.component.ts
+++ src/app/pages/client/clientLayouts/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'client-header',
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @HostListener('window:scroll', ['$event'])
 
+  onWindowScroll(e) {
+    const element = document.querySelector('.navbar');
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.remove('bg-transparent');
+      element.classList.add('bg-dark');
+    } else {
+      element.classList.remove('bg-dark');
+      element.classList.add('bg-transparent');
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
--- src/app/pages/client/home/product-section/product-section.component.ts
+++ src/app/pages/client/home/product-section/product-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-product-section',
@@ -6,8 +6,117 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-section.component.scss']
 })
 export class ProductSectionComponent implements OnInit {
+  options = {
+    items: 7, dots: false, nav: false,
+    navText: [ '<i class="fa-circle-thin" style="background-color: red"></i>', '<i class="fa-circle-thin" style="background-color: red"></i>' ], responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      1280: {
+        items: 4,
+      },
+      1568: {
+        items: 5
+      },
+      1800: {
+        items: 7
+      }
+    }
+  }
+  products = [
+    {
+      img: 'assets/img/Bananas.png',
+      title: 'Chuối',
+      des: ''
+    },
+    {
+      img: 'assets/img/Lemon.png',
+      title: 'Chanh',
+      des: 'Ưu điểm của vôi Ba Tư trong nông nghiệp thương mại so với vôi chủ chốt là ' +
+        'gian bảo quản quả lâu hơn. Chúng ít có tính axit hơn chanh chính và không có' +
+        'vị đắng cho mượn hương vị độc đáo của vôi. Chanh Ba Tư được thương mại hóa chủ yếu trong sáu kích thước, được gọi là 110, 150, 175, 200, 230 và 250'
+    },
+    {
+      img: 'assets/img/passion_fruit.png',
+      title: 'Passion Fruit',
+      des: 'Ưu điểm của vôi Ba Tư trong nông nghiệp thương mại so với vôi chủ chốt là ' +
+        'gian bảo quản quả lâu hơn. Chúng ít có tính axit hơn chanh chính và không có' +
+        'vị đắng cho mượn hương vị độc đáo của vôi. Chanh Ba Tư được thương mại hóa chủ yếu trong sáu kích thước, được gọi là 110, 150, 175, 200, 230 và 250'
+    },
+    {
+      img: 'assets/img/cattle-1.png',
+      title: 'Gia súc',
+      des: 'Ưu điểm của vôi Ba Tư trong nông nghiệp thương mại so với vôi chủ chốt là ' +
+        'gian bảo quản quả lâu hơn. Chúng ít có tính axit hơn chanh chính và không có' +
+        'vị đắng cho mượn hương vị độc đáo của vôi. Chanh Ba Tư được thương mại hóa chủ yếu trong sáu kích thước, được gọi là 110, 150, 175, 200, 230 và 250'
+    },
+    {
+      img: 'assets/img/teaktree.png',
+      title: 'Cây teak',
+      des: 'Ưu điểm của vôi Ba Tư trong nông nghiệp thương mại so với vôi chủ chốt là ' +
+        'gian bảo quản quả lâu hơn. Chúng ít có tính axit hơn chanh chính và không có' +
+        'vị đắng cho mượn hương vị độc đáo của vôi. Chanh Ba Tư được thương mại hóa chủ yếu trong sáu kích thước, được gọi là 110, 150, 175, 200, 230 và 250'
+    },
+    {
+      img: 'assets/img/cassava.png',
+      title: 'Sắn',
+      des: 'Ưu điểm của vôi Ba Tư trong nông nghiệp thương mại so với vôi chủ chốt là ' +
+        'gian bảo quản quả lâu hơn. Chúng ít có tính axit hơn chanh chính và không có' +
+        'vị đắng cho mượn hương vị độc đáo của vôi. Chanh Ba Tư được thương mại hóa chủ yếu trong sáu kích thước, được gọi là 110, 150, 175, 200, 230 và 250'
+    },
+    {
+      img: 'assets/img/coffee.png',
+      title: 'Cà phê',
+      des: 'Ưu điểm của vôi Ba Tư trong nông nghiệp thương mại so với vôi chủ chốt là ' +
+        'gian bảo quản quả lâu hơn. Chúng ít có tính axit hơn chanh chính và không có' +
+        'vị đắng cho mượn hương vị độc đáo của vôi. Chanh Ba Tư được thương mại hóa chủ yếu trong sáu kích thước, được gọi là 110, 150, 175, 200, 230 và 250'
+    },
+    {
+      img: 'assets/img/cocoa.png',
+      title: 'Ca cao',
+      des: 'Ưu điểm của vôi Ba Tư trong nông nghiệp thương mại so với vôi chủ chốt là ' +
+        'gian bảo quản quả lâu hơn. Chúng ít có tính axit hơn chanh chính và không có' +
+        'vị đắng cho mượn hương vị độc đáo của vôi. Chanh Ba Tư được thương mại hóa chủ yếu trong sáu kích thước, được gọi là 110, 150, 175, 200, 230 và 250'
+    },
+    {
+      img: 'assets/img/papaya.png',
+      title: 'Đu dủ',
+      des: 'Ưu điểm của vôi Ba Tư trong nông nghiệp thương mại so với vôi chủ chốt là ' +
+        'gian bảo quản quả lâu hơn. Chúng ít có tính axit hơn chanh chính và không có' +
+        'vị đắng cho mượn hương vị độc đáo của vôi. Chanh Ba Tư được thương mại hóa chủ yếu trong sáu kích thước, được gọi là 110, 150, 175, 200, 230 và 250'
+    },
+    {
+      img: 'assets/img/rubber-tree.png',
+      title: 'Đu dủ',
+      des: 'Ưu điểm của vôi Ba Tư trong nông nghiệp thương mại so với vôi chủ chốt là ' +
+        'gian bảo quản quả lâu hơn. Chúng ít có tính axit hơn chanh chính và không có' +
+        'vị đắng cho mượn hương vị độc đáo của vôi. Chanh Ba Tư được thương mại hóa chủ yếu trong sáu kích thước, được gọi là 110, 150, 175, 200, 230 và 250'
+    },
+    {
+      img: 'assets/img/product1.png',
+      title: 'Đu dủ',
+      des: 'Ưu điểm của vôi Ba Tư trong nông nghiệp thương mại so với vôi chủ chốt là ' +
+        'gian bảo quản quả lâu hơn. Chúng ít có tính axit hơn chanh chính và không có' +
+        'vị đắng cho mượn hương vị độc đáo của vôi. Chanh Ba Tư được thương mại hóa chủ yếu trong sáu kích thước, được gọi là 110, 150, 175, 200, 230 và 250'
+    },
+    {
+      img: 'assets/img/Grapes.png',
+      title: 'Đu dủ',
+      des: 'Ưu điểm của vôi Ba Tư trong nông nghiệp thương mại so với vôi chủ chốt là ' +
+        'gian bảo quản quả lâu hơn. Chúng ít có tính axit hơn chanh chính và không có' +
+        'vị đắng cho mượn hương vị độc đáo của vôi. Chanh Ba Tư được thương mại hóa chủ yếu trong sáu kích thước, được gọi là 110, 150, 175, 200, 230 và 250'
+    },
+  ]
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
