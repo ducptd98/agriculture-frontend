22ae6661d56e17d5bf7782675353a4d9d8902cc9
ducptd98 <ducptd98@gmail.com>
2020-09-17 14:40:33 +0700

    supply section

--- src/app/pages/client/home/home.module.ts
+++ src/app/pages/client/home/home.module.ts
@@ -2,11 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomeComponent} from './home.component';
 import {OwlCarousel, OwlModule} from 'ngx-owl-carousel';
+import { SupplySectionComponent } from './supply-section/supply-section.component';
+import { ProductSectionComponent } from './product-section/product-section.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
+    SupplySectionComponent,
+    ProductSectionComponent,
   ],
   imports: [
     CommonModule,
---
+++ src/app/pages/client/home/supply-section/supply-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-supply-section',
+  templateUrl: './supply-section.component.html',
+  styleUrls: ['./supply-section.component.scss']
+})
+export class SupplySectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
